d592447f5b885f0e8e236701f3f92c624cb954c3
Gokul <gokulakrishnanm1998@gmail.com>
2024-03-13 18:51:33 +0530

    added logout  2.05.33

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -11,13 +11,13 @@ export default function Header(){
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-8 h-8 -rotate-90"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
             />
           </svg>
@@ -35,13 +35,13 @@ export default function Header(){
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
-              class="w-4 h-4"
+              className="w-4 h-4"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
               />
             </svg>
@@ -52,13 +52,13 @@ export default function Header(){
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
             />
           </svg>
@@ -67,12 +67,12 @@ export default function Header(){
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              class="w-6 h-6 relative top-1"
+              className="w-6 h-6 relative top-1"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
           </div>
--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,9 +1,19 @@
 import { useContext } from "react";
 import  {UserContext} from "../UserContext";
-import { Link, useParams } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function AccountPage(){
     const {ready,user}=useContext(UserContext);
+    
+    let {subpage} = useParams();
+    if (subpage===undefined){
+        subpage="profile";
+    }
+
+   async  function logout(){
+      await   axios.post("/logout")
+    }
 
     if(!ready){
         return 'Loading...';
@@ -13,16 +23,31 @@ export default function AccountPage(){
         return <Navigate to={"/login"}/>
     }
 
-    const {subpage} = useParams();
-    console.log(subpage);
+   
+
+    function linkClasses(type=null){
+        let classes=  "py-2 px-6";
+        if (type===subpage ){
+            classes+=" bg-primary text-white rounded-full";
+        }
+        return classes;
+    }
+   
+
     
     return (
         <div>
-            <nav className="w-full flex justify-center mt-8 gap-2">
-            <Link className="py-2 px-6 bg-primary text-white rounded-full" to={"/account"}>My profile </Link>
-                <Link className="py-2 px-6" to={"/account/bookings"}>My bookings </Link>
-                <Link className="py-2 px-6" to={"/account/places"}>My accommodations </Link>
+            <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+            <Link className={linkClasses("profile")} to={"/account"}>My profile </Link>
+                <Link className={linkClasses("bookings")} to={"/account/bookings"}>My bookings </Link>
+                <Link className={linkClasses("places")} to={"/account/places"}>My accommodations </Link>
             </nav>
+            {subpage==="profile" && (
+                <div className="text-center max-w-lg mx-auto" >
+                    Logged in  as {user.email} ( {user.email}) <br />
+                    <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+                     </div>
+            )}
         </div>
     );
 }
