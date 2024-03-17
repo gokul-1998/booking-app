860780b9aeb58d6a76e3c22bf5ccb4904a1de7ae
Gokul <gokulakrishnanm1998@gmail.com>
2024-03-17 08:33:39 +0530

    added till 5.19

--- client/src/PlacePage.jsx
+++ client/src/PlacePage.jsx
@@ -19,9 +19,30 @@ export default function PlacePage(){
 
     return (
         <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
-            aaa
+            
             <h1 className="text-3xl">{place.title}</h1>
             <a className="my-2 block font-semibold underline" target="_blank" href={"https://maps.google.com/?q="+place.address}>{place.address}</a>
+        <div className="grid gap-2 grid-cols-[2fr_1fr]">
+            <div>
+                {place.photos?.[0] && (
+                    <div >
+
+                        <img className="aspect-square object-cover" src={"http://localhost:4000/uploads/"+place.photos?.[0]} alt="" />
+                    </div>
+                )}
+            </div>
+            <div className="grid">
+            {place.photos?.[1] && (
+                    <img className="aspect-square object-cover" src={"http://localhost:4000/uploads/"+place.photos?.[1]} alt="" />
+                )}
+                <div className="overflow-hidden">
+
+                {place.photos?.[2] && (
+                    <img className="aspect-square object-cover relative top-2 " src={"http://localhost:4000/uploads/"+place.photos?.[2]} alt="" />
+                    )}
+                    </div>
+            </div>
+        </div>
     </div>
     )
 }
