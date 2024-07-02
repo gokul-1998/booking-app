069d83f2eeb62703c6878bcf8dd91b2e70c36427
gokull-1998 <gokulakrishnanm1998@gmail.com>
2024-07-02 17:13:59 +0530

    added more features

--- api/index.js
+++ api/index.js
@@ -100,10 +100,19 @@ app.post("/upload-by-link",async (req,res)=> {
     const {link} = req.body;
     console.log(link);
     const newName="photo"+Date.now() + ".jpg";
-   await  imageDownloader.image({
-        url:link,
-        dest: __dirname + "/uploads/" + newName
-    });
+    try{
+
+        await  imageDownloader.image({
+            url:link,
+            dest: __dirname + "/uploads/" + newName
+        });
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+    console.log(newName);
+    console.log("saved")
     res.json(newName);
 
 
@@ -119,10 +128,12 @@ app.post("/upload",photosMiddleware.array('photos',100), (req,res)=>{
         const ext=parts[parts.length-1];
         const newPath=path+"."+ext;
         fs.renameSync(path,newPath);
-        uploadedFiles.push(newPath.replace("uploads\\",""));
+        uploadedFiles.push(newPath.replace("uploads","").replace("\\","").replace("/",""));
+        console.log(uploadedFiles,"uploaded files")
 
     }
     res.json(uploadedFiles);
+
     console.log("saved files")
 });
 
